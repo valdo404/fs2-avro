1137f411f2995e137720abd9df8e2fbd5dd99c21
valdo404 <lvaldes@powerspace.com>
2020-08-13 00:00:19 +0200

    Change packages

--- src/test/scala/com/levaldo/fs2/AvroEncodeDecodeSpec.scala
+++ src/test/scala/com/levaldo/fs2/AvroEncodeDecodeSpec.scala
@@ -3,8 +3,7 @@ package com.adikteev.fs2
 import java.io.File
 
 import cats.effect.IO
-import com.adikteev.fs2.SpecificPipes
-import com.aktv.test.UserScore
+import com.aktv.test.TestMessage
 import fs2.Stream
 import org.apache.avro.file.DataFileReader
 import org.apache.avro.generic.GenericDatumReader
@@ -18,14 +17,14 @@ class AvroEncodeDecodeSpec extends Specification with IOMatchers with ResultMatc
     "test file was stored" >> {
       import scala.collection.JavaConverters._
 
-      val scoreFixtures: Seq[UserScore] =
-        Seq(new UserScore("aa", 2.0d, "fr"), new UserScore("bb", 2.0d, "uk"), new UserScore("cc", 2.0d, "toto"))
+      val scoreFixtures: Seq[TestMessage] =
+        Seq(new TestMessage("aa", 2.0d), new TestMessage("bb", 2.0d), new TestMessage("cc", 2.0d))
 
       val temp = File.createTempFile("tempfile", ".avro")
 
       val ec = ExecutionContext.global
 
-      val avroPipes = SpecificPipes.apply[IO, UserScore]
+      val avroPipes = SpecificPipes.apply[IO, TestMessage]
 
       Stream
         .emits(scoreFixtures)
@@ -40,7 +39,7 @@ class AvroEncodeDecodeSpec extends Specification with IOMatchers with ResultMatc
         .map(key => (key, reader.getMetaString(key)))
 
       metadatas must havePair(
-        "avro.schema" -> """{"type":"record","name":"UserScore","namespace":"com.aktv.test","fields":[{"name":"advertising_id","type":"string"},{"name":"conversion_rate","type":"double"},{"name":"country","type":"string"}]}"""
+        "avro.schema" -> """{"type":"record","name":"TestMessage","namespace":"com.aktv.test","fields":[{"name":"test","type":"string"},{"name":"value","type":"double"}]}"""
       )
       metadatas must havePair("avro.codec" -> "snappy")
 
--- src/test/scala/com/levaldo/fs2/PipedStreamsSpec.scala
+++ src/test/scala/com/levaldo/fs2/PipedStreamsSpec.scala
@@ -1,9 +1,8 @@
 package com.adikteev.fs2
 
 import cats.effect.IO
-import com.adikteev.fs2.SpecificPipes
-import com.aktv.test.UserScore
-import fs2.{Stream, io}
+import com.aktv.test.TestMessage
+import fs2.Stream
 import org.apache.avro.file.CodecFactory
 import org.specs2.matcher.{IOMatchers, ResultMatchers}
 import org.specs2.mutable.Specification
@@ -13,14 +12,17 @@ import scala.concurrent.ExecutionContext
 class PipedStreamsSpec extends Specification with IOMatchers with ResultMatchers {
   "stream record data using pipedinputstream" >> {
     "should be piped accordingly" >> {
-      val scoreFixtures: Seq[UserScore] =
-        Seq(new UserScore("aa1", 2.0d, "fr1"), new UserScore("aa2", 2.0d, "fr2"), new UserScore("aa3", 2.0d, "fr3"))
+      val scoreFixtures: Seq[TestMessage] =
+        Seq(
+          new TestMessage("aa1", 2.0d),
+          new TestMessage("aa2", 2.0d),
+          new TestMessage("aa3", 2.0d))
 
-      val avroPipes = SpecificPipes[IO, UserScore]
+      val avroPipes = SpecificPipes[IO, TestMessage]
 
       val ec = ExecutionContext.global
 
-      val readFileOp: IO[List[UserScore]] = Stream
+      val readFileOp: IO[List[TestMessage]] = Stream
         .emits(scoreFixtures)
         .through(avroPipes.toAvroFile(ec, CodecFactory.bzip2Codec()))
         .through(avroPipes.fromAvroFile)
